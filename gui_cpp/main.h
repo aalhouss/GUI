cf21b66f93c4418e04d37f06edd6f01d57ce698f
Ahmad Al Housseini <74513758+aalhouss@users.noreply.github.com>
2022-01-29 21:37:33 +0100

    push main header

---
+++ gui_cpp/main.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by main.rc
+//
+#define ID_TIMER                        1
+#define IDC_MYICON                      2
+#define file_Exit                       3
+#define IDM_MODE_VORG                   3
+#define IDM_MODE_EXIT                   4
+#define IDM_MODE_AKT                    5
+#define IDM_MODE_WAHL                   6
+#define IDD_MAIN_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MAIN                        107
+#define IDI_SMALL                       108
+#define IDC_MAIN                        109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDD_DIALOG2                     130
+#define IDC_RADIO1                      1000
+#define IDC_RADIO2                      1001
+#define IDC_RADIO3                      1002
+#define IDC_EDIT1                       1004
+#define IDC_BUTTON3                     1005
+#define IDC_EDIT2                       1007
+#define IDC_EDIT3                       1008
+#define IDC_BUTTON1                     1009
+#define ID_FILE_SCHLIESSEN              32771
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
