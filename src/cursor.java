4ab8051ce2a4c8350e577efbd3e4d6bfda2d4b39
Ahmad Al Housseini <ahmad.al_housseini@smail.th-koeln.de>
2022-01-31 12:36:54 +0100

    git commit -a -m "Update JDialog"

--- src/cursor.java
+++ src/cursor.java
@@ -10,38 +10,35 @@ class cursor extends JFrame implements ActionListener {
     static Label l, l1, l2;
     static JTextField tf;
     static JLabel ls;
-    static JButton bestaetig,exit;
+    static JButton bestaetig, exit;
     private int a;
     static Cursor cursor;
 
     // default constructor
-    cursor()
-    {
-        dialog=new JDialog();
+    cursor() {
+        dialog = new JDialog();
         dialog.setSize(300, 200);
         dialog.setLayout(null);
-        l=new Label("Dateiname einlesen");
-        tf=new JTextField();
-        tf.setBounds(70,60,100,40);
-        l.setBounds(70,20,130,40);
-
+        l = new Label("Dateiname einlesen. Write the name capitalised");
+        tf = new JTextField();
+        tf.setBounds(70, 60, 100, 40);
+        l.setBounds(70, 20, 130, 40);
 
 
         JButton button = new JButton("Übernehmen");
         button.setVisible(true);
-        button.setBounds(0,110,100,40);
+        button.setBounds(0, 110, 100, 40);
 
         JButton button1 = new JButton("Schließen");
         button1.setVisible(true);
-        button1.setBounds(185,110,100,40);
+        button1.setBounds(185, 110, 100, 40);
 
         //add comp
         dialog.add(button);
         dialog.add(button1);
         dialog.add(l);
         dialog.add(tf);
-         cursor = new Cursor(Cursor.HAND_CURSOR);
-
+        cursor = new Cursor(Cursor.HAND_CURSOR);
 
 
         //add action Listner
@@ -54,52 +51,54 @@ class cursor extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String buttonText = new String ( ( ( JButton ) e.getSource ( ) ).getText ( ) );
+        String buttonText = new String(((JButton) e.getSource()).getText());
 
-        if(tf.getText().equals("CROSSHAIR") && e.getActionCommand().equals("Übernehmen")){
-        dialog.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+        if (tf.getText().equals("CROSSHAIR") && e.getActionCommand().equals("Übernehmen")) {
+            dialog.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+
         }
-        if(tf.getText().equals("E_RESIZE_CURSOR") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("E_RESIZE") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.E_RESIZE_CURSOR));
         }
-        if(tf.getText().equals("HAND_CURSOR") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("HAND_CURSOR") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.HAND_CURSOR));
         }
-        if(tf.getText().equals("MOVE_CURSOR") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("MOVE_CURSOR") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.MOVE_CURSOR));
         }
-        if(tf.getText().equals("N_RESIZE") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("N_RESIZE") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.N_RESIZE_CURSOR));
         }
 
-        if(tf.getText().equals("NE_RESIZE") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("NE_RESIZE") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.NE_RESIZE_CURSOR));
         }
-        if(tf.getText().equals("NW_RESIZE") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("NW_RESIZE") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.NW_RESIZE_CURSOR));
         }
-        if(tf.getText().equals("SE_RESIZE") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("SE_RESIZE") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.SE_RESIZE_CURSOR));
         }
-        if(tf.getText().equals("SW_RESIZE") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("SW_RESIZE") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.SW_RESIZE_CURSOR));
         }
-        if(tf.getText().equals("W_RESIZE") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("W_RESIZE") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.W_RESIZE_CURSOR));
         }
-        if(tf.getText().equals("TEXT") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("TEXT") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.TEXT_CURSOR));
         }
-        if(tf.getText().equals("WAIT") && e.getActionCommand().equals("Übernehmen")){
-            dialog.setCursor(new Cursor(Cursor.WAIT_CURSOR));
+        if (tf.getText().equals("WAIT") && e.getActionCommand().equals("Übernehmen")) {
+           // dialog.setCursor(new Cursor(Cursor.WAIT_CURSOR));
+            startWaitCursor(dialog.getRootPane());
         }
-        if(tf.getText().equals("CUSTOM") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("CUSTOM") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.CUSTOM_CURSOR));
         }
-        if(tf.getText().equals("DEFAULT") && e.getActionCommand().equals("Übernehmen")){
+        if (tf.getText().equals("DEFAULT") && e.getActionCommand().equals("Übernehmen")) {
             dialog.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
         }
-        if(e.getActionCommand().equals("Schließen")){
+        if (e.getActionCommand().equals("Schließen")) {
 
             int dialogButton = JOptionPane.YES_NO_OPTION;
             JOptionPane.showConfirmDialog(null, "Would You Like to Save your Previous Work First?", "Warning", dialogButton);
@@ -109,8 +108,22 @@ class cursor extends JFrame implements ActionListener {
             }
 
         }
+        //  checking ig false Mouse Cursor file names are given ,  if yes sgowing a small alert window .
+        // long if , can be avoided with else
+        if((!tf.getText().equals("DEFAULT") || !tf.getText().equals("WAIT") || !tf.getText().equals("TEXT") || !tf.getText().equals("W_RESIZE") || !tf.getText().equals("SW_RESIZE")
+        || !tf.getText().equals("SW_RESIZE") || !tf.getText().equals("SE_RESIZE") || !tf.getText().equals("NW_RESIZE") || !tf.getText().equals("NE_RESIZE") || !tf.getText().equals("N_RESIZE")
+         || !tf.getText().equals("MOVE_CURSOR") || !tf.getText().equals("HAND_CURSOR") || !tf.getText().equals("E_RESIZE") || !tf.getText().equals("CROSSHAIR")  ) && e.getActionCommand().equals("Übernehmen")){
+            JOptionPane.showMessageDialog(null, "You Entered a falsh mouse cursor name , try again");
+        }
     }
 
     // main class
 
+
+    public static void startWaitCursor(JComponent component) {
+        RootPaneContainer root =
+                (RootPaneContainer) component.getTopLevelAncestor();
+        root.getGlassPane().setCursor(new Cursor(Cursor.WAIT_CURSOR));
+        root.getGlassPane().setVisible(true);
+    }
 }
